dda098fcb15d543d9b45298433e30d2858b26d60
osbrown1 <socknot@live.com>
2023-04-12 15:43:53 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import { player, enemy, randomEnemy, gameState, endGame, switchPlayer, attack, heal, flee, playGame } from "./js/RPG.js";
+/* eslint-disable no-unused-vars */
+import "./css/styles.css";
+import { player, enemy, randomEnemy, gameState, endGame, switchPlayer, attack, heal, flee, playGame, callTurnModal, updatePlayer, updateEnemy } from "./js/RPG.js";
 
-window.onLoad = function() {
-  document.getElementById('start').onClick = (event) => {
-    event.preventDefault();
+window.addEventListener("load", () => {
 
-    const attack = document.getElementById("")
-  }
-}
+  document.getElementById('start-turn-button').addEventListener('click', e => {
+    callTurnModal(`<div>A WILD <span class="highlighted">${enemy.name.toUpperCase()}</span> DRAWS NEAR!<div>`);
+  });
 
-playGame()
+  updatePlayer(player);
+  updateEnemy(enemy);
 
+});
+// window.onLoad = function() {
+//   const attackButton = document.getElementById("attack-button");
+//   const healButton = document.getElementById("heal-button");
+//   const fleeButton = document.getElementById("flee-button");
+//   const startButton = document.getElementById("start-button");
+  
+
+//   console.log('attackButton', attackButton)
+//   console.log('attackButton', healButton)
+//   console.log('attackButton', fleeButton)
+  
+//   attackButton.addEventListener('click', e => {
+//     console.log('clicked attackButton')
+//   });
+//   healButton.addEventListener('click', e => {
+//     console.log('clicked healButton')
+//   });
+//   fleeButton.addEventListener('click', e => {
+//     console.log('clicked fleeButton')
+//   });
+//   startButton.addEventListener('click', e => {
+//     console.log('clicked startButton')
+//   });
+//   document.getElementById('start').onClick = (event) => {
+//     event.preventDefault();
+//   }
+// }
+
+// playGame()
+
+/* eslint-disable no-unused-vars */
--- src/js/RPG.js
+++ src/js/RPG.js
@@ -1,41 +1,51 @@
+/* eslint-disable no-unused-vars */
+const pause = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
 export const player = {
   name: "You",
   health: 50,
   level: 1,
   strength: 0,
   int: 0,
-  attack: 5,
-  defense: 0,
-  gold: 0,
+  attack: 8,
+  defense: 2,
+  gold: 25,
   exp: 0
-}
+};
 
-export const enemy = {
-  name: "Bill",
-  health: 20,
-  attack: 5,
-  defense: 3,
-  gold: 10
-}
+export const createEnemy = (name, health, attack, defense, gold) => ({
+  name,
+  health,
+  attack,
+  defense,
+  gold
+});
 
 export const randomEnemy = () => {
-  const names = ["Skeleton", "Giant Skeleton", "Tiny Skeleton", "Normal Zombie", "Unusual Zombie", "Not an enemy", "Vacuum Salesman", "Ghost that haunts you", "Ghost that does not haunt you"]
+  const names = ["Regular Skeleton", "Giant Skeleton", "Tiny Skeleton", "Normal Zombie", "Unusual Zombie", "Not An Enemy", "Vacuum Salesman", "Ghost That Haunts You", "Ghost That Does Not Haunt You", "Kai: Destroyer of Worlds", "Noah: Hater of Skyrim", "Mike: Master of CSS", "BeastMaster Jake", "Loot Goblin", "Your Old College Roommate", "Sentient ChatGPT", "Santa (Mall Variant)", "Real Santa", "Billy Mays", "The Train Conductor from Polar Express", "Batman", "Mean Girl", "Your Dentist", "Debt Collector", "Jimmy John's Delivery Driver", "Friday's Code Review", "A Leggo You Just Stepped On", "Your Social Anxiety", "Your Wife's Boyfriend", "Water Bear", "Bug in This Game", "Chinese Balloon", "Global Variable", "YouTube Prankster", "HTML Enjoyer"];
   const randomName = Math.floor(Math.random() * names.length);
   const name = names[randomName];
-  const health 
-  const gold = Math.floor(Math.random() * 50 ) +5;
-}
+  const health = Math.floor(Math.random() * 50 ) + 50;
+  const attack = Math.floor(Math.random() * 12 ) + 3;
+  const defense = Math.floor(Math.random() * 10 ) + 5;
+  const gold = Math.floor(Math.random() * 50 ) + 5;
+  return createEnemy(name, health, attack, defense, gold);
+};
+
+
+export let enemy = randomEnemy();
 
 export let gameState = {
   playerTurn: true,
   gameOver: false,
-}
-console.log(gameState);
+};
+
 
 export const endGame = (message) => {
-  console.log(message)
-  gameState.gameOver = true
-}
+  console.log(message);
+  gameState.gameOver = true;
+};
 
 export const switchPlayer = () => {
   if (!gameState.playerTurn) {
@@ -43,26 +53,34 @@ export const switchPlayer = () => {
   } else {
     gameState.playerTurn = false;
   }
-}
+};
 
 export const attack = (attacker, defender) => {
-  const damage = Math.max(attacker.attack - defender.defense + Math.floor(Math.random() * 5) - 2, 0);
-  if (damage > 0 ) {
-    defender.health -= damage
+
+  let damage = (attacker.attack - defender.defense) < 0 ? 1 : (attacker.attack - defender.defense);
+  const crit = randomInt(0, 8) === 8;
+  const missed = (randomInt(0,10) > 5);
+  if (crit) {
+    damage += (damage * 2);
+  }
+  console.log('missed', missed, 'damage', damage);
+  if (!missed) {
+    defender.health -= damage;
     console.log(`${attacker.name} hit ${defender.name} for ${damage} damage`);
     return;
   } else {
-    console.log("Miss!")
+    console.log("Miss!");
   } 
-}
+};
 
 export const heal = () => {
-  player.health += Math.floor(Math.random() * 25) + 2;
-}
+  player.health += Math.floor(Math.random() * 20) + 2;
+};
 
 export const flee = () => {
-  endGame("so sad, but you're alive I guess" + `you retire with ${player.gold}`)
-}
+  player.health -= 3;
+  endGame("The very act of fleeing lost you 3 health. So sad, but if you're still alive " + `you retire with ${player.gold} gold`);
+};
 
 export const playGame = () => {
   while (!gameState.gameOver) {
@@ -70,23 +88,23 @@ export const playGame = () => {
       if (player.health <= 0) {
         endGame("Game over, you died ): " + `Enemy health = ${enemy.health}`);
       } else {
-      console.log("Your turn");
-      const input = prompt("What do you want to do? (attack/heal/flee)").trim().toLowerCase();
-    switch(input) {
-      case "attack":
-        attack(player, enemy);
-        break;
-      case "heal":
-        heal();
-        break;
-      case "flee":
-        flee();
-        break;
-      default:
-        console.log("Invalid input. Please choose between attack, heal, or flee.");
-        break;;
-    }
-      switchPlayer()
+        console.log("Your turn");
+        const input = prompt("What do you want to do? (attack/heal/flee)").trim().toLowerCase();
+        switch(input) {
+        case "attack":
+          attack(player, enemy);
+          break;
+        case "heal":
+          heal();
+          break;
+        case "flee":
+          flee();
+          break;
+        default:
+          console.log("Invalid input. Please choose between attack, heal, or flee.");
+          break;
+        }
+        switchPlayer();
       }
     }
     else {
@@ -99,13 +117,80 @@ export const playGame = () => {
       }
     }
   }
-}
+};
 
-// playGame()
+console.log('//////////////////////////////////////////////');
+console.log('RPG.js RUN');
+console.log('//////////////////////////////////////////////');
 
-// window.onload = function() {
-//   document.getElementById('start').onCLick(event) {
-//     event.preventDefault();
+export const callTurnModal = (headline) => { // (headline, player)
+  document.getElementById('modal-headline').innerHTML = headline;
+  document.querySelector('#turn-modal > .modal-body').innerHTML = `
+    <button id="attack-button">Attack</button>
+    <button id="heal-button">Heal</button>
+    <button id="flee-button">Flee</button>
+  `;
+  const attackButton = document.getElementById("attack-button");
+  const healButton = document.getElementById("heal-button");
+  const fleeButton = document.getElementById("flee-button");
+  const startButton = document.getElementById("start-button");
+  
+  // Modal Buttons
+  attackButton.addEventListener('click', () => {
+    console.log('clicked attackButton');
+    attack(player, enemy);
+    updateEnemy(enemy);
+  });
+  healButton.addEventListener('click', () => {
+    console.log('clicked healButton');
+    heal();
+    console.log(`player health: ${player.health}`);
+    updatePlayer(player);
 
-//   }
-// }
+  });
+  fleeButton.addEventListener('click', () => {
+    console.log('clicked fleeButton');
+    flee();
+    updatePlayer(player);
+  });
+  startButton.addEventListener('click', () => {
+    console.log('clicked startButton');
+  });
+  document.getElementById('turn-modal').classList.remove('obscured');
+};
+
+const updatePlayerAttributes = player => `
+  <div><span class="attribute-label">Level </span>${player.level}</div>
+  <div><span class="attribute-label">Strength:</span> ${player.strength}</div>
+  <div><span class="attribute-label">Int:</span> ${player.int}</div>
+  <div><span class="attribute-label">Attack:</span> ${player.attack}</div>
+  <div><span class="attribute-label">Health:</span> ${player.health}</div>
+  <div><span class="attribute-label">Defense:</span> ${player.defense}</div>
+  <div><span class="attribute-label">Gold:</span> ${player.gold}</div>
+  <div><span class="attribute-label">Exp:</span> ${player.exp}</div>
+`;
+
+const updateEnemyAttributes = enemy => `
+  <div><span class="attribute-label">Health </span>${enemy.health}</div>
+  <div><span class="attribute-label">Attack:</span> ${enemy.attack}</div>
+  <div><span class="attribute-label">Defense:</span> ${enemy.defense}</div>
+  <div><span class="attribute-label">Gold dropped:</span> ${enemy.gold}</div>
+`;
+
+export const updatePlayer = player => {
+  document.getElementById('player-list').innerHTML = `
+    <div class="player-list-entry">
+      <h4>${player.name}</h4>
+      ${updatePlayerAttributes(player)}
+    </div>
+    `;
+};
+export const updateEnemy = enemy => {
+  document.getElementById('enemy-list').innerHTML = `
+  <div class="player-list-entry">
+    <h4>${enemy.name}</h4>
+    ${updateEnemyAttributes(enemy)}
+  </div>
+  `;
+};
+/* eslint-disable no-unused-vars */
