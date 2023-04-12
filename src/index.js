ba8298a1d8b7df9ce781fb4eccdedcd975c5b7ba
osbrown1 <socknot@live.com>
2023-04-12 11:35:22 -0700

    progress with game, wip

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { player, enemy, randomEnemy, gameState, endGame, switchPlayer, attack, heal, flee, playGame } from "./js/RPG.js";
+
+window.onLoad = function() {
+  document.getElementById('start').onClick = (event) => {
+    event.preventDefault();
+
+    const attack = document.getElementById("")
+  }
+}
+
+playGame()
+
--- src/js/RPG.js
+++ src/js/RPG.js
@@ -1,66 +1,111 @@
-const baseCharacter = {
-  health: 100,
+export const player = {
+  name: "You",
+  health: 50,
   level: 1,
   strength: 0,
   int: 0,
+  attack: 5,
+  defense: 0,
+  gold: 0,
+  exp: 0
 }
 
-
-const monk = {
-  health: 120,
-  level: 2,
-  strength: 4,
-  int: 6,
-}
-
-const warrior = {
-  health: 120,
-  level: 2,
-  strength: 4,
-  int: 6,
-}
-
-const enemy = {
+export const enemy = {
+  name: "Bill",
   health: 20,
   attack: 5,
   defense: 3,
-
+  gold: 10
 }
 
-let gameState = {
-  gameOver: false
+export const randomEnemy = () => {
+  const names = ["Skeleton", "Giant Skeleton", "Tiny Skeleton", "Normal Zombie", "Unusual Zombie", "Not an enemy", "Vacuum Salesman", "Ghost that haunts you", "Ghost that does not haunt you"]
+  const randomName = Math.floor(Math.random() * names.length);
+  const name = names[randomName];
+  const health 
+  const gold = Math.floor(Math.random() * 50 ) +5;
 }
 
-const endGame = () => {
-  console.log("Game OVeR )': !!! o': ")
+export let gameState = {
+  playerTurn: true,
+  gameOver: false,
+}
+console.log(gameState);
+
+export const endGame = (message) => {
+  console.log(message)
   gameState.gameOver = true
 }
 
-function createCharacter () {
-
-  const createWarrior = () => {
-    return { ...baseCharacter, ...warrior };
+export const switchPlayer = () => {
+  if (!gameState.playerTurn) {
+    gameState.playerTurn = true;
+  } else {
+    gameState.playerTurn = false;
   }
-
-  return { ...baseCharacter, ...wisard };
 }
 
-
-const attack = (attacker, defender) => {
-  const damage = attacker.attack - defender.defense
+export const attack = (attacker, defender) => {
+  const damage = Math.max(attacker.attack - defender.defense + Math.floor(Math.random() * 5) - 2, 0);
   if (damage > 0 ) {
     defender.health -= damage
-    console.log(`Hit for ${damage} damage`)
+    console.log(`${attacker.name} hit ${defender.name} for ${damage} damage`);
+    return;
   } else {
     console.log("Miss!")
-  }
+  } 
 }
 
+export const heal = () => {
+  player.health += Math.floor(Math.random() * 25) + 2;
+}
 
-const playGame = () => {
+export const flee = () => {
+  endGame("so sad, but you're alive I guess" + `you retire with ${player.gold}`)
+}
+
+export const playGame = () => {
   while (!gameState.gameOver) {
-
+    if (gameState.playerTurn) {
+      if (player.health <= 0) {
+        endGame("Game over, you died ): " + `Enemy health = ${enemy.health}`);
+      } else {
+      console.log("Your turn");
+      const input = prompt("What do you want to do? (attack/heal/flee)").trim().toLowerCase();
+    switch(input) {
+      case "attack":
+        attack(player, enemy);
+        break;
+      case "heal":
+        heal();
+        break;
+      case "flee":
+        flee();
+        break;
+      default:
+        console.log("Invalid input. Please choose between attack, heal, or flee.");
+        break;;
+    }
+      switchPlayer()
+      }
+    }
+    else {
+      if (enemy.health <= 0) {
+        endGame("Enemy Vanquished! " + "You win! " + `Your current health = ${player.health} :O`);
+      } else {
+        console.log("Enemy turn");
+        attack(enemy, player);
+        switchPlayer();
+      }
+    }
   }
 }
 
+// playGame()
 
+// window.onload = function() {
+//   document.getElementById('start').onCLick(event) {
+//     event.preventDefault();
+
+//   }
+// }
